3fd325b7509b1e8d0252a553d2bdc171f8cae3c6
p894g897 <patrick.gauding@ku.edu>
2019-07-06 17:41:05 -0500

    retitle; stop making string before export

--- R-savecurrent.R
+++ R-savecurrent.R
@@ -1,7 +1,7 @@
 ## Patrick Gauding <patrick.gauding@gmail.com>
 ## Last edited: 2019-07-06
 
-## R packages save and reinstall script
+## R packages save current version before update
 
 ## Save the matrix and extract the necessary list
 obj <- as.data.frame(installed.packages())
@@ -11,11 +11,8 @@ targ <- as.list(obj[,1])
 targ <- unlist(targ)
 targ <- levels(targ)
 
-## Collapse into a string
-z <- paste(targ, collapse = ", ")
-
 ## Get current R version
 current <- paste(version$major, version$minor, sep = ".")
 
 ## Write out to utilities directory
-write.table(z, file = paste("installedPackages", current, sep="-"))
+write.table(targ, file = paste("installedPackages", current, sep="-"))

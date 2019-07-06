8eea42293b0db3e246a318401d7ced7c8657b171
p894g897 <patrick.gauding@ku.edu>
2019-07-06 17:23:25 -0500

    extract a list of installed packages

--- Rpackages.R
+++ Rpackages.R
@@ -3,3 +3,13 @@
 
 ## R packages save and reinstall script
 
+## Save the matrix and extract the necessary list
+obj <- as.data.frame(installed.packages())
+
+## Not attractive but functional
+targ <- as.list(obj[,1])
+targ <- unlist(targ)
+targ <- levels(targ)
+
+## Collapse into a string
+z <- paste(targ, collapse = ", ")

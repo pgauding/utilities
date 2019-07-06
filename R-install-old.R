7da1b089d9c2608686701edf52d3fd62635e63ef
p894g897 <patrick.gauding@ku.edu>
2019-07-06 17:49:03 -0500

    read in old list; vestigial attempt at creating string

--- R-install-old.R
+++ R-install-old.R
@@ -2,3 +2,9 @@
 ## Last edited: 2019-07-06
 
 ## R packages reinstall packages from previous version of R
+
+## Import
+z <- read.table("installedPackages")
+
+## Collapse into a string
+zz <- paste(z, collapse = ", ")
